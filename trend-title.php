829ea1cd90341c0ee0121d39da386ca30aaf43a9
Susanto <50844272+susantocode@users.noreply.github.com>
2020-08-07 14:22:14 +0700

    Update trend-title.php

--- trend-title.php
+++ trend-title.php
@@ -9,7 +9,7 @@ function resourceWeb($url){
      return $hasil;
 }
 $sumber =  resourceWeb('https://stocksup.com/en/trends/fdax');
-$split = explode('<div id=""#app > div.app.ui.container > div.main-content > section > div.trend-current > div.trend-current__grid-wrapper > div.trend-current__grid > div.trend-current-summary-container > div > div.trend-current-summary__titles"">', $sumber);
+$split = explode('<div id="#app > div.app.ui.container > div.main-content > section > div.trend-current > div.trend-current__grid-wrapper > div.trend-current__grid > div.trend-current-summary-container > div > div.trend-current-summary__titles">', $sumber);
 $splitLagi = explode('</div>', $split[1]);
 
 echo $splitLagi[0];
