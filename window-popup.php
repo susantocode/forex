d093929d65d3698152809548e7ba9d76acab6b21
Susanto <50844272+susantocode@users.noreply.github.com>
2020-08-07 15:23:34 +0700

    Update and rename trend-title.php to window-popup.php

---
+++ window-popup.php
@@ -0,0 +1,7 @@
+<script language="JavaScript">
+<!--
+function popupWindow(URL){
+	NewWindow = window.open(URL, name, 'width=311,height=330,location=no,menubar=no,resizable=no,scrollbars=no,toolbar=no')
+	}
+//-->
+</script>
